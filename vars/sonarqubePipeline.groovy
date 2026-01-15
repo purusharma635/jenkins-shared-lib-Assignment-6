a40b00aef968cebb1e33e4e163e8c102ae13fa78
Puru Sharma <purusharma635@gmail.com>
2026-01-15 13:29:30 +0530

    Create sonarqubePipeline.groovy

---
+++ vars/sonarqubePipeline.groovy
@@ -0,0 +1,45 @@
+def call(Map config = [:]) {
+
+    pipeline {
+        agent any
+
+        stages {
+
+            stage('Clone Repository') {
+                steps {
+                    echo "Cloning SonarQube code..."
+                    git config.REPO_URL
+                }
+            }
+
+            stage('User Approval') {
+                when {
+                    expression { config.KEEP_APPROVAL_STAGE == true }
+                }
+                steps {
+                    input message: "Do you want to run SonarQube playbook?"
+                }
+            }
+
+            stage('Playbook Execution') {
+                steps {
+                    echo "Running Ansible playbook for SonarQube..."
+                    sh """
+                    ansible-playbook ${config.CODE_BASE_PATH}/sonarqube.yml \
+                    -i ${config.CODE_BASE_PATH}/inventory
+                    """
+                }
+            }
+
+            stage('Notification') {
+                steps {
+                    echo "Sending Slack Notification..."
+                    slackSend(
+                        channel: config.SLACK_CHANNEL_NAME,
+                        message: config.ACTION_MESSAGE
+                    )
+                }
+            }
+        }
+    }
+}
