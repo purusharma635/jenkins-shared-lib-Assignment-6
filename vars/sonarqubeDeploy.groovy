71ac14c395017d715df034bf6672363df0a99b43
Puru Sharma <purusharma635@gmail.com>
2026-01-18 18:30:05 +0530

    Update sonarqubeDeploy.groovy

--- vars/sonarqubeDeploy.groovy
+++ vars/sonarqubeDeploy.groovy
@@ -1,45 +1,26 @@
-def call(Map config = [:]) {
+def call(Map config) {
 
-    pipeline {
-        agent any
+    stage('Clone Repo') {
+        git 'https://github.com/purusharma635/sonarqube-ansible-Assignment-6.git'
+    }
 
-        stages {
-
-           stage('Clone Repository') {
-               steps {
-                   echo "Cloning SonarQube code..."
-                   git branch: 'main', url: config.REPO_URL
-              }
-         }
-
-            stage('User Approval') {
-                when {
-                    expression { config.KEEP_APPROVAL_STAGE == true }
-                }
-                steps {
-                    input message: "Do you want to run SonarQube playbook?"
-                }
-            }
-
-            stage('Playbook Execution') {
-                steps {
-                    echo "Running Ansible playbook for SonarQube..."
-                    sh """
-                    ansible-playbook ${config.CODE_BASE_PATH}/sonarqube.yml \
-                    -i ${config.CODE_BASE_PATH}/inventory
-                    """
-                }
-            }
-
-            stage('Notification') {
-                steps {
-                    echo "Sending Slack Notification..."
-                    slackSend(
-                        channel: config.SLACK_CHANNEL_NAME,
-                        message: config.ACTION_MESSAGE
-                    )
-                }
-            }
+    if (config.KEEP_APPROVAL_STAGE) {
+        stage('User Approval') {
+            input message: "Deploy SonarQube to ${config.ENVIRONMENT}?"
         }
+    }
+
+    stage('Playbook Execution') {
+        sh """
+        ansible-playbook install-sonarqube.yml \
+        -i inventory/${config.ENVIRONMENT}
+        """
+    }
+
+    stage('Notification') {
+        slackSend(
+          channel: config.#ninja-devops-course-,
+          message: config.ACTION_MESSAGE
+        )
     }
 }
